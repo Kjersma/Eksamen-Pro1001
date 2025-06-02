1563e89a97c7fe837ff2f366008247a9a9089fef
Kjersma <kmagnussen.design@gmail.com>
2025-06-02 09:43:15 +0200

    Counter + mobile
    Lagt til handlekurv-teller og en del endringer til mobilvennlig site. Fikset menyen.

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,32 @@
-const toggleButton = document.querySelector('.hamburger');
-const mobileMenu   = document.querySelector('.dropdown-menu');
 
-toggleButton.addEventListener('click', () => {
-  mobileMenu.open = !mobileMenu.open;
-});
+ const toggleButton = document.querySelector('.hamburger');
+  const closeButton  = document.querySelector('.close');
+  const mobileMenu   = document.querySelector('.dropdown-menu');
+   const cartButton   = document.querySelector('.green-button');
+     const addButtons = document.querySelectorAll('.add-to-basket');
 
-window.onkeydown = function (event) {
-  if (event.key == "Escape") {
+  // Åpne/lukke dialogen når hamburger-knappen klikkes
+  toggleButton.addEventListener('click', () => {
+    mobileMenu.open = !mobileMenu.open;
+  });
+
+  // Escape
+  window.addEventListener('keydown', (event) => {
+    if (event.key === "Escape") {
+      mobileMenu.open = false;
+    }
+  });
+
+  // ×"-knappen
+  closeButton.addEventListener('click', () => {
     mobileMenu.open = false;
-   }
-};
+  });
 
-
-
-
+// Handlekurv
+  let cartCount = 0;
+  addButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        cartCount += 1;
+        cartButton.textContent = cartCount;
+      });
+    });
